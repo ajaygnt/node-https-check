1f13371190c7546b8fe9472aa0da077ef176b1a7
Ajay Krishna <ajay@resonantcloud.info>
2021-09-13 13:27:33 +0530

    four

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use('/', (req, res) => {
-    console.log(req.protocal);
-    console.log(req.IncomingMessage.encrypted);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n'+req.secure+'\n'+req.IncomingMessage.encrypted);
+    res.end('Hello World\n'+req.headers['x-forwarded-proto']);
 })
 app.listen(port, () => console.log(`Listening on port ${port}...`));

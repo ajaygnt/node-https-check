1d01945223db95273caf559a34ee937da0f18679
Ajay Krishna <ajay@resonantcloud.info>
2021-09-13 13:10:05 +0530

    third commit

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use('/', (req, res) => {
     console.log(req.protocal);
     console.log(req.IncomingMessage.encrypted);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n'+req.secure);
+    res.end('Hello World\n'+req.secure+'\n'+req.IncomingMessage.encrypted);
 })
 app.listen(port, () => console.log(`Listening on port ${port}...`));
